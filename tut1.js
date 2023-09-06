b0621ac99e67ad30a07e5f4a630ad2668210c3f2
MetaSystemMWI <128566079+MetaSystemMWI@users.noreply.github.com>
2023-09-06 16:57:23 +0500

    THIS IS  JAVA SCRIPT TUTORIAL

---
+++ tut1.js
@@ -0,0 +1,33 @@
+// document.write("Hello World");
+// console.log("Hello World!");
+// angular
+// react
+// jquery
+// Vue.js
+// ext.js
+// ember.js
+// meteor
+// mithril
+// node.js
+// polymer
+// aurelia
+// backbone.js
+// ecma script 2005-2019, 2021,2023
+
+// function sayHello(){
+//     alert("hello world")
+// }
+
+// var money;
+// var ali;
+
+// var money, name;
+//     name="Ali Ahmed";
+//     money = 200;
+//     var a = money++;
+
+// document.write(money);
+// document.write(name);
+
+
+
